768ff65bcd8a51afef634250c517414d8a927ecd
Dakson38 <daksonvibes38@gmail.com>
2025-03-12 20:41:32 -0400

    style: adding prettier

--- src/main/java/com/jose/kardex/KardexApplication.java
+++ src/main/java/com/jose/kardex/KardexApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KardexApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KardexApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(KardexApplication.class, args);
+  }
 }
